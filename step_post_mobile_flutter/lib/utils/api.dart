c43705e1d8177df28fb9ad98782c716ef2fe5fc4
CyrilPonsan <cyril.ponsan@gmail.com>
2022-11-21 08:48:04 +0100

    intercept

--- step_post_mobile_flutter/lib/utils/api.dart
+++ step_post_mobile_flutter/lib/utils/api.dart
@@ -1,6 +1,6 @@
 class API {
-  //final String _url = "http://dev01.step.eco:3000/api";
-  final String _url = "https://step-post-nodejs.herokuapp.com/api";
+  final String _url = "http://dev01.step.eco:3000/api";
+  //final String _url = "https://step-post-nodejs.herokuapp.com/api";
 
   String get url => _url;
 }
