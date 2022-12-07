9e7898b22070bd1dcc804e7e8f242865194a95e5
CyrilPonsan <cyril.ponsan@gmail.com>
2022-12-07 08:50:56 +0100

    welcome.png ok

--- step_post_mobile_flutter/lib/views/widgets/welcome_widget.dart
+++ step_post_mobile_flutter/lib/views/widgets/welcome_widget.dart
@@ -8,28 +8,19 @@ class WelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 50),
+      margin: const EdgeInsets.only(top: 100),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
         children: [
           Image.asset(
-            "assets/images/welcome.webp",
-            width: MediaQuery.of(context).size.width * .75,
+            "assets/images/welcome.png",
+            //width: MediaQuery.of(context).size.width * .75,
             fit: BoxFit.cover,
           ),
           const SizedBox(
             height: 16,
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .75,
-            child: const CustomText(
-              label:
-                  "Bonjour, scannez un QRCode ou recherchez un numéro de bordereau avec le formulaire",
-              size: 18,
-              hasAlignment: TextAlign.justify,
-            ),
-          )
         ],
       ),
     );
