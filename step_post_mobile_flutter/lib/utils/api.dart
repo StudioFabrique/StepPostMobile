c85532a2c40b3a72bffa1a0f0ef6d1ad3940052e
CyrilPonsan <cyril.ponsan@gmail.com>
2022-12-08 16:35:28 +0100

    deploy

--- step_post_mobile_flutter/lib/utils/api.dart
+++ step_post_mobile_flutter/lib/utils/api.dart
@@ -1,6 +1,6 @@
 class API {
   //final String _url = "https://dev01.step.eco/api";
-  final String _url = "https://flutter-post.osc-fr1.scalingo.io/api";
+  final String _url = "https://test-api.step-post.fr/api";
 
   String get url => _url;
 }
