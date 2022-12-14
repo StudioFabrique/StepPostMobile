64f71aa409863884390b8d51fad663157e7d4b83
CyrilPonsan <cyril.ponsan@gmail.com>
2022-12-14 09:57:52 +0100

    mes scans

--- step_post_mobile_flutter/lib/utils/api.dart
+++ step_post_mobile_flutter/lib/utils/api.dart
@@ -1,6 +1,6 @@
 class API {
-  //final String _url = "https://test-api.step-post.fr/api";
-  final String _url = "http://192.168.1.15:3000/api";
+  final String _url = "https://test-api.step-post.fr/api";
+  //final String _url = "http://192.168.1.32:3000/api";
 
   String get url => _url;
 }
